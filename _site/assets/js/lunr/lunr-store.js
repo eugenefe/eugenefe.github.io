5a6c57330555eb4b12ce74927d439e152178fe45
arazorback <arazorback@localhost.localdomain>
2023-04-18 02:08:59 +0900

    dev_stage3

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,73 @@
-var store = []
+var store = [{
+        "title": "Readme",
+        "excerpt":"Box Model IFRS 17에서는 부채를 시가로 평가하며, 직전 결산부터 당 결산까지 부채의 변동을 보험금융비용과 보험수익항목 및 CSM의 변동금액으로 설명함. GMV는 관측하고자 하는 변동스텝 별 이행현금흐름을 Cash flow단위로 평가하는 모델로써, 개별 현금흐름단위에서 변동스텝별로 인식할 회계적 항목 (보험금융비용과 보험수익(비용), CSM 증감분 등)을 식별함. 포트폴리오 별 회계정책에 따라 혹은 현금흐름의 속성(미래 서비스 관련...","categories": [],
+        "tags": [],
+        "url": "/gmv/README/",
+        "teaser": null
+      },{
+        "title": "Csm",
+        "excerpt":"Result of Box : GoC GoC단위의 Box Result 계약 집합 단위에서 금액이 확정되는 CSM 이나 Loss 및 직접신계약비 (DAC) 항목은 개별 CF단위에서 산출한 결과를 바탕으로 GoC (계약집합) level에서 집계된 금액의 부호 및 손실요소 배부비율 등을 결정할 수 있다. CSM Loss 손실요소가 발생한 GoC의 손실요소 추적관련 처리사항. 신계약의 비용인식, 손실요소 배부,...","categories": [],
+        "tags": [],
+        "url": "/gmv/csm/",
+        "teaser": null
+      },{
+        "title": "Csmall",
+        "excerpt":"Result of Box : GoC GoC단위의 Box Result 계약 집합 단위에서 금액이 확정되는 CSM 이나 Loss 및 직접신계약비 (DAC) 항목은 개별 CF단위에서 산출한 결과를 바탕으로 GoC (계약집합) level에서 집계된 금액의 부호 및 손실요소 배부비율 등을 결정할 수 있다. CSM Loss 손실요소가 발생한 GoC의 손실요소 추적관련 처리사항. 신계약의 비용인식, 손실요소 배부,...","categories": [],
+        "tags": [],
+        "url": "/gmv/csm/csmAll/",
+        "teaser": null
+      },{
+        "title": "Result of Box : CF",
+        "excerpt":"EPV , FACE_AMT, TVOM, FCOST , AOCI 등 프로젝션된 예상 CF를 기반으로 미래의 각 시점 별 Cash Flow단위에서 산출된 각각의 계산 결과를 Box결과로 지칭함. CF 단위 Box Result /gmv/epv/ {“title”=&gt;”GMV”, “children”=&gt;nil}{“title”=&gt;”Box Model”, “url”=&gt;”/gmv/README/”}{“title”=&gt;”Result of Box”, “url”=&gt;”/gmv/epv/”}{“title”=&gt;”csm”, “url”=&gt;”/gmv/csm/”}{“title”=&gt;”zzz”, “children”=&gt;[{“title”=&gt;”csm All”, “url”=&gt;”/gmv/csm/csmAll/”}]} 예를 들어 특정 t+1시점의 보험요소 관련 현금흐름 항목을 평가할 때,...","categories": [],
+        "tags": [],
+        "url": "/gmv/epv/",
+        "teaser": null
+      },{
+        "title": "Baz Boom Identity",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/portfolio/baz-boom-identity/",
+        "teaser": "/assets/images/unsplash-gallery-image-1-th.jpg"
+      },{
+        "title": "Fizz Bang Identity",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/portfolio/fizz-bang-identity/",
+        "teaser": "/assets/images/unsplash-gallery-image-2-th.jpg"
+      },{
+        "title": "Foo Bar Identity",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/portfolio/foo-bar-website/",
+        "teaser": "/assets/images/foo-bar-identity-th.jpg"
+      },{
+        "title": "Ginger Gulp Identity",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/portfolio/ginger-gulp-identity/",
+        "teaser": "/assets/images/unsplash-gallery-image-3-th.jpg"
+      },{
+        "title": "Edge Case: Nested and Mixed Lists",
+        "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that Lists within lists do not break the ordered list numbering order Your list styles go deep enough. Ordered – Unordered – Ordered ordered item ordered item unordered unordered ordered item ordered item ordered item...","categories": ["Edge Case"],
+        "tags": ["content","css","edge case","lists","markup"],
+        "url": "/edge%20case/2009/05/15/edge-case-nested-and-mixed-lists.html",
+        "teaser": null
+      },{
+        "title": "Edge Case: Many Tags",
+        "excerpt":"This post has many tags.  ","categories": ["Edge Case"],
+        "tags": ["8BIT","alignment","Articles","captions","categories","chat","comments","content","css","dowork","edge case","embeds","excerpt","Fail","featured image","FTW","Fun","gallery","html","image","Jekyll","layout","link","Love","markup","Mothership","Must Read","Nailed It","Pictures","Post Formats","quote","standard","Success","Swagger","Tags","template","title","twitter","Unseen","video","YouTube"],
+        "url": "/edge%20case/2009/06/01/edge-case-many-tags.html",
+        "teaser": null
+      },{
+        "title": "Edge Case: Many Categories",
+        "excerpt":"This post has many categories.  ","categories": ["aciform","antiquarianism","arrangement","asmodeus","broder","buying","championship","chastening","disinclination","disinfection","dispatch","echappee","enphagy"],
+        "tags": ["categories","edge case"],
+        "url": "/aciform/antiquarianism/arrangement/asmodeus/broder/buying/championship/chastening/disinclination/disinfection/dispatch/echappee/enphagy/2009/07/02/edge-case-many-categories.html",
+        "teaser": null
+      },{
+        "title": "Edge Case: No Body Content",
+        "excerpt":" ","categories": ["Edge Case"],
+        "tags": ["content","edge case","layout"],
+        "url": "/edge%20case/2009/08/06/edge-case-no-body-content.html",
+        "teaser": null
+      },]
